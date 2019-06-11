e5002825065ab233b49f0ebe7962b91b8fd364a5
Nikola Miljkovic <nikomil@gmail.com>
2019-06-10 20:48:38 -0500

    Added callbacks for specific data

--- README.md
+++ README.md
@@ -33,9 +33,54 @@ Gps::Device.open '/dev/ttyUSB0' do |device|
   end
 
   # Fetch coordinates from sentences as they come through (converted to decimal format)
-  device.each_coordinate do |coords|
+  device.each_coordinates do |coords|
     puts "Last known timestamp: #{device.last_datetime}" unless device.last_datetime.nil?
     puts "Location: #{coords}"
   end
 end
+```
+
+Using callbacks for specific data
+```ruby
+device = Gps::Device.new 'nmea_data.txt'
+
+device.on_coordinates do |coords|
+  puts "Location: #{coords}"
+end
+
+device.on_datetime do |datetime|
+  puts "Date: #{datetime.to_s}"
+end
+
+device.on_speed_knots do |speed|
+  puts "Speed: #{speed} knots"
+end
+
+device.on_speed_kmh do |speed|
+  puts "Speed: #{speed} kmh"
+end
+
+device.on_altitude do |altitude|
+  puts "Altitude: #{altitude} meters"
+end
+
+device.on_true_track do |true_track|
+  puts "True track: #{true_track}"
+end
+
+device.on_magnetic_track do |mag_track|
+  puts "Magnetic track: #{mag_track}"
+end
+
+device.on_complete do
+  puts "Done!"
+end
+
+# Synchronous
+device.start
+
+# Asynchronous
+device.rewind
+thread = device.start_async
+thread.join
 ```
--- lib/gps/device.rb
+++ lib/gps/device.rb
@@ -3,9 +3,16 @@ module Gps
     attr_accessor :last_datetime # Updates any time a sentence has a datetime
 
     def initialize(filename, track_datetime=true)
+      @callbacks = {}
       @track_datetime = track_datetime
       @filename = filename
       @file = File.open(filename, 'rb')
+    end
+
+    def self.open(filename, &block)
+      device = Device.new filename
+      block.call device
+      device.close
     end
 
     def close
@@ -29,19 +36,123 @@ module Gps
       end
     end
 
-    def each_coordinate(&block)
+    def each_coordinates(&block)
       each_sentence do |sentence|
         block.call sentence.lat_long_dec if sentence.has_coordinates?
       end
     end
 
-    def self.open(filename, &block)
-      gps_file = Device.new filename
-      block.call gps_file
-      gps_file.close
+    # Rewind file if not a character device
+    def rewind
+      @file.rewind unless File.chardev?(@file.path)
+    end
+
+    # Begin fetch process with callbacks
+    def start
+      @stopped = false
+      each_sentence do |sentence|
+        break if @stopped
+
+        if @callbacks[:coordinates] && sentence.has_coordinates?
+          @callbacks[:coordinates].each do |block|
+            block.call sentence.lat_long_dec
+          end
+        end
+
+
+        if @callbacks[:datetime] && sentence.has_datetime?
+          @callbacks[:datetime].each do |block|
+            block.call sentence.datetime
+          end
+        end
+
+        if @callbacks[:speed_knots] && sentence.respond_to?(:speed_knots)
+          @callbacks[:speed_knots].each do |block|
+            block.call sentence.speed_knots if sentence.speed_knots
+          end
+        end
+
+        if @callbacks[:speed_kmh] && sentence.respond_to?(:speed_kmh)
+          @callbacks[:speed_kmh].each do |block|
+            block.call sentence.speed_kmh if sentence.speed_kmh
+          end
+        end
+
+        if @callbacks[:altitude] && sentence.respond_to?(:altitude)
+          @callbacks[:altitude].each do |block|
+            block.call sentence.altitude if sentence.altitude
+          end
+        end
+
+        if @callbacks[:true_track] && sentence.respond_to?(:true_track)
+          @callbacks[:true_track].each do |block|
+            block.call sentence.true_track if sentence.true_track
+          end
+        end
+
+        if @callbacks[:magnetic_track] && sentence.respond_to?(:magnetic_track)
+          @callbacks[:magnetic_track].each do |block|
+            block.call sentence.magnetic_track if sentence.magnetic_track
+          end
+        end
+      end
+
+      if @callbacks[:complete]
+        @callbacks[:complete].each(&:call)
+      end
+    end
+
+    def start_async
+      Thread.new(&method(:start))
+    end
+
+    def stop
+      @stopped = true
+    end
+
+    def on_coordinates(&block)
+      callbacks[:coordinates] ||= []
+      callbacks[:coordinates] << block
+    end
+
+    def on_datetime(&block)
+      callbacks[:datetime] ||= []
+      callbacks[:datetime] << block
+    end
+
+    def on_speed_knots(&block)
+      callbacks[:speed_knots] ||= []
+      callbacks[:speed_knots] << block
+    end
+
+    def on_speed_kmh(&block)
+      callbacks[:speed_kmh] ||= []
+      callbacks[:speed_kmh] << block
+    end
+
+    def on_altitude(&block)
+      callbacks[:altitude] ||= []
+      callbacks[:altitude] << block
+    end
+
+    def on_complete(&block)
+      callbacks[:complete] ||= []
+      callbacks[:complete] << block
+    end
+
+    def on_true_track(&block)
+      callbacks[:true_track] ||= []
+      callbacks[:true_track] << block
+    end
+
+    def on_magnetic_track(&block)
+      callbacks[:magnetic_track] ||= []
+      callbacks[:magnetic_track] << block
     end
 
     private
+
+    attr_reader :callbacks
 
     def update_datetime(sentence)
       if sentence.respond_to? :datetime
